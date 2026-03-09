e246dff5e442469dbcd115629a2b47f18a037a1e
Tarso Virgilio <tarsovirgilioc@gmail.com>
2026-03-09 15:59:58 -0300

    feat: implements Layout and changes in page.tsx removing what was implemented in layout.tsx. Created cache for find all posts to avoid repeated call

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,9 +1,12 @@
+import { findAllPublicPosts } from '@/lib/post/queries';
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
+import { PostSummary } from '../PostSummary';
 
-export function PostFeatured() {
-  const slug = 'qualquer';
-  const postLink = `/post/${slug}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPosts();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}`;
 
   return (
     <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
@@ -14,31 +17,19 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: '/images/bryen_9.png',
-          alt: 'Alt da imagem',
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
 
-      <div className='flex flex-col gap-4 sm:justify-center'>
-        <time
-          className='text-slate-600 block text-sm/tight'
-          dateTime='2025-04-20'
-        >
-          20/04/2025 10:00
-        </time>
-
-        <PostHeading as='h1' url={postLink}>
-          Rerum, vel ex? Impedit ullam harum blanditiis
-        </PostHeading>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit
-          dolorem est dolor porro, doloribus neque, quidem mollitia doloremque,
-          ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis
-          mollitia?
-        </p>
-      </div>
+      <PostSummary
+        postLink={postLink}
+        postHeading='h1'
+        createdAt={post.createdAt}
+        excerpt={post.excerpt}
+        title={post.title}
+      />
     </section>
   );
 }
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,14 +1,13 @@
-import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
-import { formatDatetime, formatDistanceToNow } from '@/utils/format-datetime';
+import { PostSummary } from '../PostSummary';
+import { findAllPublicPosts } from '@/lib/post/queries';
 
 export async function PostsList() {
-  const posts = await postRepository.findAll();
+  const posts = await findAllPublicPosts();
 
   return (
     <div className='grid grid-cols-1  gap-8 sm:grid-cols-2 lg:grid-cols-3'>
-      {posts.map(post => {
+      {posts.slice(1).map(post => {
         const postLink = `/post/${post.slug}`;
 
         return (
@@ -25,21 +24,13 @@ export async function PostsList() {
               }}
             />
 
-            <div className='flex flex-col gap-4 sm:justify-center'>
-              <time
-                className='text-slate-600 block text-sm/tight'
-                dateTime={post.createdAt}
-                title={formatDistanceToNow(post.createdAt)}
-              >
-                {formatDatetime(post.createdAt)}
-              </time>
-
-              <PostHeading as='h2' url={postLink}>
-                {post.title}
-              </PostHeading>
-
-              <p>{post.excerpt}</p>
-            </div>
+            <PostSummary
+              postLink={postLink}
+              postHeading='h2'
+              createdAt={post.createdAt}
+              excerpt={post.excerpt}
+              title={post.title}
+            />
           </div>
         );
       })}
